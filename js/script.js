973ff0b527758c2adac7ede5ea9de3f1d6a113a8
Guz117 <cristian.guzman117@gmail.com>
2021-12-01 17:36:01 +0100

    addEventListener

--- js/script.js
+++ js/script.js
@@ -9,33 +9,41 @@ const colCrazy = 7;
 
 const container = document.querySelector('.container')
 
-// for (let i = 1; i < 101; i++) {
-//     const square = document.createElement('div');
-//     square.classList.add('square');
-//     square.style.width = `calc(100% / ${colEasy})`;
-//     square.style.height = `calc(100% / ${rowEasy})`;
-//     square.append(i);
-//     container.append(square);
-// }
 
-// for (let i = 1; i < 65; i++) {
-//     const square = document.createElement('div');
-//     square.classList.add('square');
-//     square.style.width = `calc(100% / ${colNormal})`;
-//     square.style.height = `calc(100% / ${rowNormal})`;
-//     square.append(i);
-//     container.append(square);
-// }
+const easySelection = document.getElementById('select').value = 'easy';
+const normalSelection = document.getElementById('select').value = 'normal';
+const crazySelection = document.getElementById('select').value = 'crazy';
+const button = document.getElementById('play');
 
-
-for (let i = 1; i < 50; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.style.width = `calc(100% / ${colCrazy})`;
-    square.style.height = `calc(100% / ${rowCrazy})`;
-    square.append(i);
-    container.append(square);
-}
-
-
+button.addEventListener('click',
+  function () {
+    if (easySelection) {
+        for (let i = 1; i < 101; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${colEasy})`;
+            square.style.height = `calc(100% / ${rowEasy})`;
+            square.append(i);
+            container.append(square);
+        }
+    } else if (normalSelection){
+        for (let i = 1; i < 65; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${colNormal})`;
+            square.style.height = `calc(100% / ${rowNormal})`;
+            square.append(i);
+            container.append(square);
+        }
+    } else if (crazySelection) {
+        for (let i = 1; i < 50; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(100% / ${colCrazy})`;
+            square.style.height = `calc(100% / ${rowCrazy})`;
+            square.append(i);
+            container.append(square);
+        }
+    }
+  });
 
