a5b51bd215ed8539c928cdc42b194746760de770
Guz117 <cristian.guzman117@gmail.com>
2021-12-01 16:16:25 +0100

    grid con js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
+const row = 10;
+const col = 10;
+const container = document.querySelector('.container')
+
+for (let i = 1; i < 101; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.width = `calc(100% / ${col})`;
+    square.style.height = `calc(100% / ${row})`;
+    square.append(i);
+    container.append(square);
+}
 
 
