556725d30c303d5c24096ba0caf022ca7010fea9
Guz117 <cristian.guzman117@gmail.com>
2021-12-02 12:09:29 +0100

    aggiunta selezione difficoltà e blu click

--- js/script.js
+++ js/script.js
@@ -1,46 +1,51 @@
 const rowEasy = 10;
 const colEasy = 10;
 
-const rowNormal = 8;
-const colNormal = 8;
+const rowNormal = 9;
+const colNormal = 9;
 
 const rowCrazy = 7;
 const colCrazy = 7;
 
-const container = document.querySelector('.container')
-
-
-// const easySelection = document.getElementById('select').value = 'easy';
-// const normalSelection = document.getElementById('select').value = 'normal';
-// const crazySelection = document.getElementById('select').value = 'crazy';
+const container = document.querySelector('.container');
 const button = document.getElementById('play');
 
 button.addEventListener('click',
   function () {
-     let selection = document.getElementById('select').value;
+    container.innerHTML = '';
+    let selection = document.getElementById('select').value;
     console.log(selection)
-     if (selection == 'Easy') {
-        for (let i = 1; i < 101; i++) {
+    if (selection == 'Easy') {
+        for (let i = 1; i <= 100; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('click', function () {
+                this.style.background = 'blue';
+            } );
             square.style.width = `calc(100% / ${colEasy})`;
             square.style.height = `calc(100% / ${rowEasy})`;
             square.append(i);
             container.append(square);
         }
     } else if (selection == 'Normal'){
-        for (let i = 1; i < 65; i++) {
+        for (let i = 1; i <= 81; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('click', function () {
+                this.style.background = 'blue';
+            } );
             square.style.width = `calc(100% / ${colNormal})`;
             square.style.height = `calc(100% / ${rowNormal})`;
             square.append(i);
             container.append(square);
         }
     } else if (selection == 'Crazy') {
-        for (let i = 1; i < 50; i++) {
+        for (let i = 1; i <= 49; i++) {
             const square = document.createElement('div');
             square.classList.add('square');
+            square.addEventListener('click', function () {
+                this.style.background = 'blue';
+            } );
             square.style.width = `calc(100% / ${colCrazy})`;
             square.style.height = `calc(100% / ${rowCrazy})`;
             square.append(i);
